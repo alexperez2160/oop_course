73f8168fad24de9e4ea2ad691e5fde339c258c9f
alexperez2160 <alexperez2160@gmail.com>
2016-06-23 00:30:42 +0000

    Finished chapter 1

--- oop_book/chapter_1.rb
+++ oop_book/chapter_1.rb
@@ -1,5 +1,31 @@
 # question 1 
-class Man
+# class Man
+#   include
+# end 
+
+# bobby = Man.new
+
+# Bobby is an object or instance of the class Man.
+
+# Question 2
+# A module is a collection of behaviors that is useable in other classes via mixins.
+
+module Smell 
+  def smell(aroma)
+    puts "smelling #{aroma}."
+  end 
+end
+
+class Dog
+  include Smell
 end 
 
-bobby = Man.new
+class Man
+  include Smell
+end 
+
+bobby = Man.new 
+bobby.smell("fart")
+
+spot = Dog.new
+spot.smell("doodoo")
